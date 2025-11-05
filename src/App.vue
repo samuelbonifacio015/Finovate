b9e54c833dd7edd53043ab672f867a2dc0b2ee6b
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-11-05 11:31:40 -0500

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppNavbar from "@/shared/components/AppNavbar.vue";
+import AppFooter from "@/shared/components/AppFooter.vue";
 
 const handleMenuToggle = () => {
   console.log('Menu toggled')
@@ -9,6 +10,7 @@ const handleMenuToggle = () => {
 <template>
   <div>
     <app-navbar @menu-toggle="handleMenuToggle"></app-navbar>
+    <app-footer @menu-toggle="handleMenuToggle"></app-footer>
     <router-view></router-view>
   </div>
 </template>
---
+++ src/shared/components/AppFooter.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer class="layout-footer">
+    <div class="footer-container">
+      <div class="footer-content">
+        <a
+            href="https://github.com/samuelbonifacio015/Finovate"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="footer-link"
+        >
+          <button class="p-link layout-footer-button">
+            <i class="pi pi-github"></i>
+          </button>
+          <span>Ver en GitHub</span>
+        </a>
+        <div class="footer-text">
+          <p class="footer-copy">© 2025 Finovate</p>
+          <p class="footer-desc">Una aplicación de gestión financiera</p>
+          <p class="footer-desc">desarrollada por Samuel Bonifacio</p>
+        </div>
+        <div class="footer-balance"></div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/styles/components/_footer.scss";
+</style>
